0f4e273d64d612d91d3d9931e4e43abaa06fe342
unknown <deepak.281295@gmail.com>
2024-06-09 12:59:04 +0530

    Added query 1

---
+++ AdventureWorks01.sql
@@ -0,0 +1,12 @@
+USE AdventureWorks2019;
+
+/*
+Question 1:
+
+Write a SQL Statement that will give you a count of each object type in the AdventureWorks database. Order by count descending.
+*/
+
+SELECT type_desc, COUNT(*) AS Object_Count
+FROM sys.objects
+GROUP BY type_desc
+ORDER BY 2 DESC;
